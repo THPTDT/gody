1e6bc651d4fe8a3b8744b0649fd8e7d8c23f4ece
minhtricute69 <minhtricute69@gmail.com>
2021-05-18 08:52:07 +0700

    Thêm màn hình Login

--- gody/lib/login_screen.dart
+++ gody/lib/login_screen.dart
@@ -8,7 +8,30 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset('assets/images/logo.png', height: 70.0),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 30, 0, 30)),
+              Text(
+                "ĐĂNG NHẬP",
+                style: TextStyle(
+                  fontFamily: 'GoogleSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
